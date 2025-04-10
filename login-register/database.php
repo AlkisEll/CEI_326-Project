0a20d6cbce970ac178ad901166b8fae9fddf441e
AlkisEll <ac.ellinides@edu.cut.ac.cy>
2025-04-10 20:20:55 +0300

    database.php

--- login-register/database.php
+++ login-register/database.php
@@ -1,9 +1,9 @@
 <?php
 
-$hostName = "localhost";
-$dbUser = "root";
-$dbPassword = "";
-$dbName = "login_register";
+$hostName = "cei326-omada7.cut.ac.cy";
+$dbUser = "cei326omada7user";
+$dbPassword = "DCXf!2OTgG!cYy52";
+$dbName = "cei326omada7";
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
  
 if (!$conn) {
