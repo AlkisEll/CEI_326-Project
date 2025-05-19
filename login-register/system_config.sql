a68f26f1605d5ee3d96a30c01da30025c9d23631
Elias <eliassolomonides0@gmail.com>
2025-05-19 20:05:35 +0300

    Delete login-register directory

--- login-register/system_config.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE system_config (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    config_key VARCHAR(100) NOT NULL,
-    config_value TEXT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY (config_key)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
