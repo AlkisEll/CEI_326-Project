c57204170bbb859eeb55cbfc72e8e6a27f6a9555
Elias <eliassolomonides0@gmail.com>
2025-05-19 21:05:57 +0300

    Update google_callback.php

--- special_scientists/google_callback.php
+++ special_scientists/google_callback.php
@@ -8,7 +8,7 @@ require_once "database.php";
 
 $clientID = '298135741411-6gbhvfmubpk1vjgbeervmma5mntarggk.apps.googleusercontent.com';
 $clientSecret = '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';
-$redirectUri = 'http://localhost/special-scientists/google_callback.php';
+$redirectUri = 'http://cei326-omada7.cut.ac.cy/special-scientists/google_callback.php';
 
 if (!isset($_GET['code'])) {
     header('Location: registration.php');
